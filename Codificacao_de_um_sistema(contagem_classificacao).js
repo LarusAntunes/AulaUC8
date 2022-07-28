766ae700de6371d9adb4e195f31ee7c54421d9f4
Larus <larusantunes.luz@hotmail.com>
2022-07-28 10:19:53 -0300

    atividade 3

---
+++ Codificacao_de_um_sistema(contagem_classificacao).js
@@ -0,0 +1,16 @@
+
+let numeroDeAlunos = 10
+
+for (let contador = 0; contador < numeroDeAlunos; contador++) {
+    
+    console.log(contador)
+
+    if (contador == 0) {
+        console.log ('classificado o zero')
+    }
+            if (contador % 2 == 0) {
+                console.log('numero par')
+            }else {
+                console.log('numero impar')
+            }
+}
