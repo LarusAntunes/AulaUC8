ee2a589da0abe039b4e093df1fbaf4fbcae3f256
Larus <larusantunes.luz@hotmail.com>
2022-08-02 16:18:39 -0300

    Estudos dia 02

---
+++ Desafio_2.js
@@ -0,0 +1,7 @@
+// guardar uma lista de estudantes com o nome de cada aluno
+// apresentar nome de cada aluno inserido
+//o nome só podera ser inserido caso a quantidade de alunos<5
+
+let listaEstudantes = ["Helena", "Chico", "Mario"];
+let quantidadeEstudantes = listaEstudantes.length;
+console.log(quantidadeEstudantes);
